1178cf5bb9e96fb9995b5716969990188f8c0f6d
osum0001 <osum0001@student.monash.edu>
2026-05-21 10:48:59 +1000

    First Draft

---
+++ vegalitescript.js
@@ -0,0 +1,11 @@
+var vg_1 = "BarChart.vg.json";
+
+vegaEmbed("#bar_chart", vg_1)
+
+var vg_2 = "HorizontalBarChart.vg.json"
+
+vegaEmbed("#hozbar_chart", vg_2)
+
+var vg_3 = "rainfall-map.vg.json"
+
+vegaEmbed("#rainfall_map", vg_3)
